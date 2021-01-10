d101f3e1c8fb77a695c78b2d79bd2cab2d286849
Roberto Rocha <rrocha.roberto@gmail.com>
2021-01-10 13:15:44 -0300

    Fix the tests config and process.

---
+++ student-functional-test/src/test/java/br/edu/rocha/student/functional/prod/HealthCheckITCase.java
@@ -0,0 +1,49 @@
+package br.edu.rocha.student.functional.prod;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HealthCheckITCase {
+
+	private static String baseURL;
+
+	@BeforeClass
+	public static void setup() throws FileNotFoundException, IOException {
+		baseURL = System.getProperty("base.server.url");
+		if (baseURL == null) {
+			baseURL = "http://localhost:4200/students";
+		}
+	}
+
+	@Test
+	public void healthCheck() throws MalformedURLException {
+		WebDriver driver = prepareDriver();
+		try {
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+	}
+
+	private WebDriver prepareDriver() {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu", "--silent");
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.navigate().to(baseURL);
+		return driver;
+	}
+}
