d93cef636652a786a86891fd9c1581ee28154ec7
Roberto Rocha <rrocha.roberto@gmail.com>
2021-01-10 13:17:25 -0300

    Show version in the app.

--- student-frontend/src/app/app.component.ts
+++ student-frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { StudentService } from './student.service';
 
+import { environment } from '../environments/environment.prod';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,8 @@ import { StudentService } from './student.service';
 })
 export class AppComponent implements OnInit {
   studService: StudentService;
+
+  public version: string = environment.version;
 
   constructor (studService: StudentService) {
     this.studService = studService;
